72deec1ce9a2708e4e5ad6100675156a7f454250
mlatjac <mlatjac@miguellegault.org>
2019-11-27 21:29:49 +0000

    Apply fixes from StyleCI

--- tests/BjrHiTest.php
+++ tests/BjrHiTest.php
@@ -6,10 +6,11 @@ use Orchestra\Testbench\TestCase;
 
 class BjrHiTest extends TestCase
 {
-	// protected function getPackageProviders($app) // To included providers required by test
-	/** @test */
-	public function it_just_works()
-	{
-		$this->assertTrue(true);
-	}
+    // protected function getPackageProviders($app) // To included providers required by test
+
+    /** @test */
+    public function it_just_works()
+    {
+        $this->assertTrue(true);
+    }
 }
